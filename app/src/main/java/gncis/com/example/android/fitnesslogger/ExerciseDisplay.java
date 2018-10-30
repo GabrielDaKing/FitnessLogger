fd0391ceb3e5af74c8140f2e2deeabf6b3157896
Samreen <samreenreyaz@outlook.com>
2018-10-30 06:32:21 +0530

    Rename

--- app/src/main/java/gncis/com/example/android/fitnesslogger/ExerciseDisplay.java
+++ app/src/main/java/gncis/com/example/android/fitnesslogger/ExerciseDisplay.java
@@ -8,6 +8,6 @@ public class ExerciseDisplay extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exersize_display);
+        setContentView(R.layout.activity_exercise_display);
     }
 }
